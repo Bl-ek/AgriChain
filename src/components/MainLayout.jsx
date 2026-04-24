ce255978f502470aae360559f0e73ff8dabbdd06
mighty808 <effahaboagye888@gmail.com>
2026-04-24 19:47:48 +0000

    add MainLayout component for layout structure with Sidebar and TopHeader

---
+++ src/components/MainLayout.jsx
@@ -0,0 +1,14 @@
+import { Sidebar } from './Sidebar'
+import { TopHeader } from './TopHeader'
+
+export function MainLayout({ children, title, breadcrumb }) {
+  return (
+    <div className="min-h-screen bg-white">
+      <Sidebar />
+      <TopHeader title={title} breadcrumb={breadcrumb} />
+      <main className="p-8">
+        {children}
+      </main>
+    </div>
+  )
+}
